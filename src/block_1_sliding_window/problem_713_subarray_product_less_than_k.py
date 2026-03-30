895ef2ad6de3c12bf23f596176ada77116565d71
Sergii Paliutin <spalutin@flutterint.com>
2026-03-30 13:38:09 +0100

    fifth problem

---
+++ src/block_1_sliding_window/problem_713_subarray_product_less_than_k.py
@@ -0,0 +1,29 @@
+"""
+Given an array of integers nums and an integer k,
+return the number of contiguous subarrays
+where the product of all the elements in the subarray is strictly less than k.
+
+"""
+
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: list[int], k: int) -> int:
+        left = 0
+        right = 0
+        product = 1
+        total = 0
+
+        while right < len(nums):
+
+            product *= nums[right]
+
+            while left < len(nums) and left < right and product >= k:
+                product = product // nums[left]
+                left += 1
+
+            if product < k:
+                total += right - left + 1
+
+            right += 1
+
+        return total
---
+++ src/block_1_sliding_window/test_problem_713_subarray_product_less_than_k.py
@@ -0,0 +1,14 @@
+from .problem_713_subarray_product_less_than_k import Solution
+
+fixtures = [
+    [[10, 5, 2, 6], 100, 8],
+    [[1, 2, 3], 0, 0],
+    [[1,2,3,4,5], 1, 0]
+]
+
+
+def test_num_subarray_product_less_than_k():
+    print()
+    for nums, k, expected in fixtures:
+        print(nums, k, expected)
+        assert Solution().numSubarrayProductLessThanK(nums, k) == expected
