e83fa6b6812d7718a6ca254b556cc6fb5fbe7189
Sergey Palutin <spalutin@flutterint.com>
2026-05-06 14:29:10 +0100

    problem 15 3sum tests

--- src/block_2_two_pointers/test_problem_15_3sum.py
+++ src/block_2_two_pointers/test_problem_15_3sum.py
@@ -4,11 +4,14 @@ fixtures = [
     [[-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]],
     [[0, 1, 1], []],
     [[0, 0, 0], [[0, 0, 0]]],
+
+    [[-1, 0, 1], [[-1, 0, 1]]],
+    [[1, 2, -2, -1], []],
 ]
 
 
 def test_three_sum(debug=True):
     for [nums, expected] in fixtures:
         if debug: print()
-        if debug: print(nums, expected)
+        if debug: print(nums, '->', expected)
         assert sorted(sorted(xx) for xx in Solution().threeSum(nums)) == sorted(sorted(xx) for xx in expected)
