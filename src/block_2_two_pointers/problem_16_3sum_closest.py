820f494ffa5eaaba57312d06ac7b5565da8f1e76
Sergey Palutin <spalutin@flutterint.com>
2026-05-06 16:33:37 +0100

    problem 16 3sum closest - task

---
+++ src/block_2_two_pointers/problem_16_3sum_closest.py
@@ -0,0 +1,20 @@
+"""
+Given an integer array `nums` of length `n` and an integer `target`,
+find three integers at **distinct indices** in `nums` such that the sum is closest to target.
+
+Return __the sum of the three integers.__
+
+You may assume that each input would have exactly one solution.
+
+Constraints:
+
+3 <= nums.length <= 500
+-1000 <= nums[i] <= 1000
+-10^4 <= target <= 10^4
+"""
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        return 0
---
+++ src/block_2_two_pointers/test_problem_16_3sum_closest.py
@@ -0,0 +1,8 @@
+fixtures = [
+    [[-1, 2, 1 - 4], 1, 2],
+    [[0, 0, 0], 1, 0]
+]
+
+
+def test_three_sum_closest():
+    assert False
