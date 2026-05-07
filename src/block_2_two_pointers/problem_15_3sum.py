039b2e1c456b49fd7300bc4c7a0a182f0892b6c0
Sergey Palutin <spalutin@flutterint.com>
2026-05-07 17:07:16 +0100

    problem 16 3sum closest - solved

--- src/block_2_two_pointers/problem_15_3sum.py
+++ src/block_2_two_pointers/problem_15_3sum.py
@@ -13,6 +13,41 @@ Constraints:
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         result = []
+        xx = sorted(nums)
+        ha = 0
+        kz = len(nums) - 1
+
+        while kz - ha > 1:
+            while kz - ha > 2 and xx[ha] + xx[ha + 1] + xx[kz] > 0:
+                kz -= 1
+
+            ix = ha + 1
+            jy = kz
+
+            while ix < jy:
+                value = xx[ha] + xx[ix] + xx[jy]
+
+                if value == 0:
+                    result.append([xx[ha], xx[ix], xx[jy]])
+
+                if value < 0:
+                    while ix < jy and xx[ix] == xx[ix + 1]:
+                        ix += 1
+                    ix += 1
+                else:
+                    while jy > ix and xx[jy] == xx[jy - 1]:
+                        jy -= 1
+                    jy -= 1
+
+            while ha < kz and xx[ha] == xx[ha + 1]:
+                ha += 1
+
+            ha += 1
+
+        return result
+
+    def threeSumOld(self, nums: list[int]) -> list[list[int]]:
+        result = []
         entries: dict[int, bool] = {}
 
         for x in nums:
