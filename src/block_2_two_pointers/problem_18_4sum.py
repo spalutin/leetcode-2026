d3ee7cd497c536c481bd6eedf28eebb3574c08d6
Sergey Palutin <spalutin@flutterint.com>
2026-05-08 16:13:59 +0100

    problem 18 4sum - done

--- src/block_2_two_pointers/problem_18_4sum.py
+++ src/block_2_two_pointers/problem_18_4sum.py
@@ -20,12 +20,23 @@ class Solution:
     def fourSum(self, nums: list[int], target: int) -> list[list[int]]:
         result = set()
 
-        size = len(nums)
         xx = sorted(nums)
         hw = 0
-        while size - hw > 2:
-            kz = size - 1
+        max_kz = len(nums) - 1
+
+        while max_kz - hw > 2:
+            if xx[hw] + xx[hw + 1] + xx[hw + 2] + xx[hw + 3] > target:
+                break
+
+            while max_kz - hw > 3:
+                if xx[hw] + xx[hw + 1] + xx[hw + 2] + xx[max_kz - 1] < target:
+                    break
+                max_kz -= 1
+
+            kz = max_kz
             while kz - hw > 2:
+                if xx[kz - 3] + xx[kz - 2] + xx[kz - 1] + xx[kz] < target:
+                    break
                 ix = hw + 1
                 jy = kz - 1
                 while ix < jy:
