c361891d98cab1e772014683fb1128ec262a0085
Sergey Palutin <spalutin@flutterint.com>
2026-05-11 17:35:16 +0100

    problem 876

--- src/block_3_hare_tortoise/problem_141_linked_list_cycle.py
+++ src/block_3_hare_tortoise/problem_141_linked_list_cycle.py
@@ -6,12 +6,9 @@ from block_3_hare_tortoise.list_node import ListNode
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         fast = slow = head
-        should_move_slow = False
-        while fast:
-            fast = fast.next
-            if slow and should_move_slow:
-                slow = slow.next
-            should_move_slow = not should_move_slow
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
             if fast == slow:
                 return True
         return False
---
+++ src/block_3_hare_tortoise/problem_876_middle_of_the_linked_list.py
@@ -0,0 +1,18 @@
+# [1] -> [1]
+# [1, 2] -> [2]
+# [1, 2, 3] -> [2, 3]
+# [1, 2, 3, 4] -> [3, 4]
+# [1, 2, 3, 4, 5] -> [3, 4, 5]
+# [1, 2, 3, 4, 5, 6] -> [4, 5, 6]
+from typing import Optional
+
+from block_3_hare_tortoise.list_node import ListNode
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
