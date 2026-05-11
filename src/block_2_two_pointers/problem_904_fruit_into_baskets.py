4a2dd98e8f3e14d14d71eb8d6924b06aace539b7
Sergey Palutin <spalutin@flutterint.com>
2026-05-11 16:05:16 +0100

    problem 904

--- src/block_2_two_pointers/problem_904_fruit_into_baskets.py
+++ src/block_2_two_pointers/problem_904_fruit_into_baskets.py
@@ -19,32 +19,32 @@ from pygments.lexers import j
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         size = len(fruits)
-        if size < 3:
-            return size
+
         ix = 0
         jy = 0
+
         x = fruits[0]
-        y = fruits[1]
-        while y == x and jy < size:
-            y = fruits[jy]
+        y = fruits[0]
+
+        while jy < size - 1 and y == x:
             jy += 1
+            y = fruits[jy]
 
         kz = jy + 1
         result = kz - ix
 
         while kz < size:
-
             z = fruits[kz]
-
             if z != fruits[jy]:
                 if z != x and z != y:
                     if kz - ix > result:
                         result = kz - ix
-                    x = fruits[jy]
-                    y = fruits[kz]
                     ix = jy
-
-                jy = kz
+                    jy = kz
+                    x = fruits[ix]
+                    y = fruits[jy]
+                else:
+                    jy = kz
 
             kz += 1
 
