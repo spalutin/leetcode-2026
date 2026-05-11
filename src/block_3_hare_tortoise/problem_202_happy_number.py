e247f3fe3c7ae748449cba27c20e9b69286b9cf7
Sergey Palutin <spalutin@flutterint.com>
2026-05-11 17:52:40 +0100

    problem 202

--- src/block_3_hare_tortoise/problem_202_happy_number.py
+++ src/block_3_hare_tortoise/problem_202_happy_number.py
@@ -1,11 +1,17 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        visited = set()
-        while n > 1 and n not in visited:
-            visited.add(n)
-            m = 0
-            while n > 0:
-                m += (n % 10) ** 2
-                n = n // 10
-            n = m
-        return n == 1
+        slow = n
+        fast = Solution.getSum(n)
+        while fast != slow:
+            slow = Solution.getSum(slow)
+            fast = Solution.getSum(fast)
+            fast = Solution.getSum(fast)
+        return slow == 1
+
+    @staticmethod
+    def getSum(n: int) -> int:
+        m = 0
+        while n > 0:
+            m += (n % 10) ** 2
+            n = n // 10
+        return m
