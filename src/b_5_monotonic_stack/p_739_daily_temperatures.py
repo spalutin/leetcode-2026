e086498b3b30ef8d2acad2386c177df357515d02
SergeyPalutin <spalutin@flutterint.com>
2026-06-22 17:43:49 +0100

    monotonic stack daily temperatures simplifying

--- src/b_5_monotonic_stack/p_739_daily_temperatures.py
+++ src/b_5_monotonic_stack/p_739_daily_temperatures.py
@@ -1,21 +1,19 @@
 """
 https://leetcode.com/problems/daily-temperatures/
 """
-from typing import List, Tuple
+from typing import List
 
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack: List[Tuple[int, int]] = []
+        stack: List[int] = []
         result = [0] * len(temperatures)
 
         for ix in range(len(temperatures)):
-            x = temperatures[ix]
-
-            while stack and stack[-1][1] < x:
-                jy, _ = stack.pop()
+            while stack and temperatures[stack[-1]] < temperatures[ix]:
+                jy = stack.pop()
                 result[jy] = ix - jy
 
-            stack.append((ix, x))
+            stack.append(ix)
 
         return result
